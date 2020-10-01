c3398511eaf6230e7938f6d046d9ba8328157322
Sharpienero <sharpienero@gmail.com>
2020-10-01 14:26:01 -0400

    Finish boat-validation

--- src/components/DealerForm.vue
+++ src/components/DealerForm.vue
@@ -313,9 +313,15 @@
               label-cols-sm="3"
               label="Year:"
               label-align-sm="right"
-              label-for="nested-boat"
+              label-for="nested-year"
             >
-              <b-form-input id="nested-boat"></b-form-input>
+              <b-form-input id="nested-year" v-model="$v.boat_year.$model">
+              </b-form-input>
+              <span
+                v-if="!$v.boat_year.required && $v.boat_year.$dirty"
+                class="text-danger"
+                >Boat Year is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -324,7 +330,15 @@
               label-align-sm="right"
               label-for="nested-length"
             >
-              <b-form-input id="nested-length"></b-form-input>
+              <b-form-input
+                id="nested-length"
+                v-model="$v.boat_length.$model"
+              ></b-form-input>
+              <span
+                v-if="!$v.boat_length.required && $v.boat_length.$dirty"
+                class="text-danger"
+                >Boat Length is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -333,7 +347,15 @@
               label-align-sm="right"
               label-for="nested-make"
             >
-              <b-form-input id="nested-make"></b-form-input>
+              <b-form-input
+                id="nested-make"
+                v-model="$v.boat_make.$model"
+              ></b-form-input>
+              <span
+                v-if="!$v.boat_make.required && $v.boat_make.$dirty"
+                class="text-danger"
+                >Boat Make is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -342,7 +364,10 @@
               label-align-sm="right"
               label-for="nested-doc-num"
             >
-              <b-form-input id="nested-doc-num"></b-form-input>
+              <b-form-input
+                id="nested-doc-num"
+                v-model="$v.boat_doc.$model"
+              ></b-form-input>
             </b-form-group>
 
             <b-form-group
@@ -350,36 +375,23 @@
               label="Boat is kept at:"
               label-align-sm="right"
             >
-              <b-dropdown id="dropdown-header" text="Dropdown" class="m-2">
-                <b-dropdown-header id="dropdown-header-label">
-                  Boat is kept at:
-                </b-dropdown-header>
-                <b-dropdown-item-button
-                  aria-describedby="dropdown-header-label"
-                >
-                  Marina
-                </b-dropdown-item-button>
-                <b-dropdown-item-button
-                  aria-describedby="dropdown-header-label"
-                >
-                  Home Dock
-                </b-dropdown-item-button>
-                <b-dropdown-item-button
-                  aria-describedby="dropdown-header-label"
-                >
-                  Trailer
-                </b-dropdown-item-button>
-                <b-dropdown-item-button
-                  aria-describedby="dropdown-header-label"
-                >
-                  Mooring
-                </b-dropdown-item-button>
-                <b-dropdown-item-button
-                  aria-describedby="dropdown-header-label"
-                >
-                  Other
-                </b-dropdown-item-button>
-              </b-dropdown>
+              <b-form-select
+                v-model="$v.boat_kept_at.$model"
+                :options="boat_kept_at_options"
+                class="mb-3"
+              >
+                <!-- This slot appears above the options from 'options' prop -->
+                <template v-slot:first>
+                  <b-form-select-option :value="null" disabled
+                    >-- Please select an option --</b-form-select-option
+                  >
+                </template>
+              </b-form-select>
+              <span
+                v-if="!$v.boat_kept_at.required && $v.boat_kept_at.$dirty"
+                class="text-danger"
+                >Boat Location is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -388,7 +400,15 @@
               label-align-sm="right"
               label-for="nested-boat-city"
             >
-              <b-form-input id="nested-boat-city"></b-form-input>
+              <b-form-input
+                id="nested-boat-city"
+                v-model="$v.boat_loc_city.$model"
+              ></b-form-input>
+              <span
+                v-if="!$v.boat_loc_city.required && $v.boat_loc_city.$dirty"
+                class="text-danger"
+                >Boat City is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -397,7 +417,15 @@
               label-align-sm="right"
               label-for="nested-boat-state"
             >
-              <b-form-input id="nested-boat-state"></b-form-input>
+              <b-form-input
+                id="nested-boat-state"
+                v-model="$v.boat_loc_state.$model"
+              ></b-form-input>
+              <span
+                v-if="!$v.boat_loc_state.required && $v.boat_loc_state.$dirty"
+                class="text-danger"
+                >Boat State is required!
+              </span>
             </b-form-group>
 
             <b-form-group
@@ -406,15 +434,25 @@
               label-align-sm="right"
               label-for="nested-country"
             >
-              <b-form-input id="nested-boat-country"></b-form-input>
+              <b-form-input
+                id="nested-boat-country"
+                v-model="$v.boat_loc_country.$model"
+              ></b-form-input>
+              <span
+                v-if="
+                  !$v.boat_loc_country.required && $v.boat_loc_country.$dirty
+                "
+                class="text-danger"
+                >Boat Country is required!
+              </span>
             </b-form-group>
           </b-form-group>
 
           <label for="by continuing">
             By continuing, the user verifies that their boats are in good
-            working order and understand that Dock-to-Dock tows from the
-            boat’s home port to a repair facility, a ramp for haul out or
-            similar, are not covered for the first 30 days after membership
+            working order and understand that Dock-to-Dock tows from the boat’s
+            home port to a repair facility, a ramp for haul out or similar, are
+            not covered for the first 30 days after membership
             activation.</label
           >
         </b-card>
