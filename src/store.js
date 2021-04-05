458ed058c304dd58df5574bd947732d846db1d53
Patrick <sharpienero@gmail.com>
2021-04-05 15:47:31 -0400

    Remove hard-coded URLs

--- src/store.js
+++ src/store.js
@@ -31,7 +31,7 @@ export default new Vuex.Store({
         login({ commit }, user) {
             return new Promise((resolve, reject) => {
                 commit('auth_request')
-                axios({ url: 'http://localhost:3000/login', data: user, method: 'POST' })
+                axios({ url:`${process.env.VUE_APP_APIURL}/auth/`, data: user, method: 'POST' })
                     .then(resp => {
                         const token = resp.data.token
                         const user = resp.data.user
@@ -51,12 +51,12 @@ export default new Vuex.Store({
         register({ commit }, user) {
             return new Promise((resolve, reject) => {
                 commit('auth_request')
-                axios({ url: 'http://localhost:3000/register', data: user, method: 'POST' })
+                axios({ url: `${process.env.VUE_APP_APIURL}/register/`, data: user, method: 'POST' })
                     .then(resp => {
                         const token = resp.data.token
                         const user = resp.data.user
                         localStorage.setItem('token', token)
-                        // Add the following line:
+                        
                         axios.defaults.headers.common['Authorization'] = token
                         commit('auth_success', token, user)
                         resolve(resp)
